17aae42685d46d9e5f947f5029f7e9d57fce8e15
Ortlieb <andy.ortlieb@it12-0297.opasc.net>
2013-08-30 00:42:35 -0500

    Can swap accounts upward, downward

--- pub/uiv2.js
+++ pub/uiv2.js
@@ -11,6 +11,35 @@ function FACtrl($scope){
 		localStorage.accounts = JSON.stringify($scope.accounts);
 	}
 
+	$scope.save = function(){
+		console.log("STUB", "Smartly select a storage device.")
+		return $scope.saveToLocalStorage();
+	}
+
+	$scope.swapup = function(idx){
+		console.log("BBUTTTTS");
+		var swap = $scope.accounts[idx];
+		console.log("dink");
+		if (idx > 0){
+			console.log("Batmanning");
+			$scope.accounts[idx] = $scope.accounts[idx-1];
+			$scope.accounts[idx-1] = swap;
+			$scope.save();
+		}
+	}
+
+	$scope.swapdown = function(idx){
+		var swap = $scope.accounts[idx];
+		console.log("dink");
+		if ($scope.accounts.length-1 > idx){
+			console.log("Batmanning");
+			$scope.accounts[idx] = $scope.accounts[idx+1];
+			$scope.accounts[idx+1] = swap;
+			$scope.save();
+		}
+	}
+
+
 	$scope.readFromLocalStorage();
 
 }
